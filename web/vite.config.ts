606d023ba6a6f0fffefcff0b1e0d3a53729f1293
roy <62500486+aazf@users.noreply.github.com>
2026-04-24 16:06:21 +0000

    fix: allow all hosts in vite dev server

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: true,
     proxy: {
       '/api': 'http://localhost:8080',
       '/present': 'http://localhost:8080',
